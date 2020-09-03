82239edd699b8fced4f78e54c5c5839947934acd
fourth06 <fawu1031@gmail.com>
2020-09-03 17:22:01 +0800

    'doIFLB'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,50 @@
 <template>
   <div>
-    <label v-for="value in list">
-        <input type="checkbox" :value="value.empno" v-model="mymymy">
-        {{value.empno}} {{value.name}}
-    </label>
+     監測編碼查詢頁面
     <br>
-    <input type="textarea" value="" v-model="message"/>
+    <button>新增</button>
+    <br>
+    監測項目: 
+    <select v-model="item">
+        <option v-for="data in listItem" :value="data.MN_COD">{{data.MN_COD}} {{data.MN_NAM}}</option>
+    </select>
+    <br>
+    檢體來源: 
+    <select v-model="source">
+        <option v-for="(value,key) in list" :value="key">{{value}}</option>
+    </select>
+    <br>
+    狀態: 
+    <input type="radio" id="normal" v-model="tsc" value="生效"/>
+    <label for="A">生效</label>
+    <input type="radio" id="middle" v-model="tsc" value="失效"/>
+    <label for="D">失效</label>
     <br>
     <button @click="getapi()">送出</button>
+    <table border="1">
+            <tr>
+                <td>#</td>
+                <td>監測編碼</td>
+                <td>監測項目</td>
+                <td>檢體來源	</td>
+                <td>生效日期</td>
+                <td>失效日期</td>
+                <td>狀態</td>
+                <td>異動人員</td>
+                <td>異動時間</td>
+            </tr>
+            <tr v-for="(data,index) in this.dataIflb">
+                    <td><button @click="changeedit(data)">編輯</button></td>
+                    <td>{{data.MN_COD}}</td>
+                    <td>{{de64base(data.MN_NAM)}}</td>
+                    <td>{{de64base(data.srcNam)}}</td>
+                    <td>{{data.EFF_DAT}}</td>
+                    <td>{{data.STP_DAT}}</td>
+                    <td>{{data.TSC}}</td>
+                    <td>{{data.RTP}}</td>
+                    <td>{{data.RTT}}</td>
+            </tr>
+        </table>
   </div>
 </template>
 
@@ -17,44 +54,47 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      mymymy: [],
-      message: '',
-      sendData: [],
-      empno: '',
-      list:[
-       { empno: "12604", name: "子靖" },
-       { empno: "12833", name: "仕龍" },
-       { empno: "12834", name: "韋豪" },
-       { empno: "12948", name: "子婷" },
-       { empno: "11087", name: "承甫" },
-       { empno: "11241", name: "仕昇" },
-       { empno: "11692", name: "冠均" },
-       { empno: "11736", name: "琪鴻" },
-       { empno: "12037", name: "志遠" },
-       { empno: "13131", name: "琦堯" },
-       { empno: "13536", name: "鴻輝" },
-       { empno: "11002", name: "筱薰" },
-       { empno: "12651", name: "志宏" },
-     ],
+      item: '',
+      source: '',
+      tsc: '',
+      list: ["水質", "器械","環境","人員"],
+      listItem: [{
+                MN_COD: 'AIRB',
+                MN_NAM: '空氣落菌',
+                },
+                {
+                MN_COD: 'BCWQ',
+                MN_NAM: '燒傷中心水質',
+
+                },
+                {
+                MN_COD: 'ERPA',
+                MN_NAM: '員工餐廳準備區',
+
+                },
+                {
+                MN_COD: 'ERTB',
+                MN_NAM: '員工餐廳餐具',
+                },
+                {
+                MN_COD: 'ERWQ',
+                MN_NAM: '員工餐廳水質'
+                },
+                {
+                MN_COD: 'ESMN',
+                MN_NAM: '內視鏡監測',
+                },
+                {
+                MN_COD: 'ESMN-G',
+                MN_NAM: '內視鏡監測(腸胃管鏡)',
+                }],
+      dodo: '',
+      dataIflb: '',
     }
   },
   methods:{
     getapi(){
-      if (this.mymymy) {
-        this.mymymy.forEach(Element =>
-          {
-            if (this.empno) {
-              this.empno += ','+Element
-            } else {
-              this.empno += Element
-            }
-          }
-        )
-      }
-      console.log(this.empno)
-      this.sendData.push({'ser_no':'000', 'typ':'1', 'receiver':this.empno, 'msg':this.message, 'sender':'12604', 'lnk_no':'', 'src':''});
-      console.log(this.sendData)
-      let tag = 'evt.msg.sendmsg'
+      let tag = 'pvt.if.getMnCodData'
       let param = new FormData()
       let conf = {
         params: {
@@ -64,20 +104,34 @@ export default {
       let obj = {
         wb_base64: 0,
         encode: "N",
-        mtyp: "13",
-        mtyp: "13",
-        msgtxt: this.sendData
+        MN_COD: this.item,
+        SRC: this.source,
+        TSC: this.tsc
       }
       param.append("var", Base64.encode(JSON.stringify(obj)));
       console.log(obj)
       this.$axios.post("/api", param, conf)
       .then((response) => {
         console.log(response);
+          this.dodo = JSON.parse(Base64.decode(response.data));
+          if (this.dodo.sts == '000000') {
+            this.dataIflb = this.dodo.val;
+          }
+          console.log(this.dataIflb);
       })
       .catch((error) => {
         console.log(error);
       })
-    }
+    },
+    de64base(str){
+      return Base64.decode(str);
+    },
+    changeedit(data){
+      this.$router.push({
+        name: 'editPage',
+        params:{'MN_NO':data.MN_NO,'MN_NAM':data.MN_NAM,'SRC':data.SRC,'TSC':data.TSC},
+      })
+    },
   },
 }
 </script>
---
+++ src/components/editPage.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        監測編碼編輯畫面
+        <br>
+        監測編碼:{{$route.params.MN_NO}}
+        <br>
+        監測項目:<input type="text" v-model='name'>
+        <br>
+        檢體來源:{{$route.params.SRC}}
+        <br>
+        狀態:{{$route.params.TSC}}
+    </div>
+</template>
+
+<script>
+import { Base64 } from 'js-base64'
+export default {
+  name: 'editPage',
+  data () {
+    return {
+        name: '',
+    }
+  },
+  methods:{
+    de64base(str){
+      return Base64.decode(str);
+    },
+  },
+  mounted() {
+      this.name = this.de64base(this.$route.params.MN_NAM)
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b9
+}
+.tttab{
+    width: 100%;
+    border:1.5px solid black;
+    display: flex;
+    flex-direction: row;
+    /* flex-wrap: wrap; */
+}
+.stack{
+    background-color: yellowgreen;
+    align:left;
+    width: 20%;
+}
+.middle{
+    flex-wrap: wrap;
+    width: 80%;
+}
+.trrrr{
+    align:left;
+    border:1px solid rgb(0, 60, 255);
+    width: 100%;
+}
+.concc{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+}
+.imgcc{
+    align:center;
+    width: 20%;
+}
+.valcc{
+    align:left;
+    width: 100%;
+}
+</style>
